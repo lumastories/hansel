e7f5e8fa555d13b68750c37aaf0d57f685167ff9
Nate Nichols <natsamnic@gmail.com>
2016-10-23 14:53:39 -0700

    Learning tests :)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App, { Login } from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+})
+
+describe('maths', () => {
+
+  it('can add', () => {
+    expect(1+2).toBe(3)
+    expect(1+2).not.toBeLessThan(0)
+  })
+
+  it('can subtract', () => {
+    expect(1-2).not.toBe(-2)
+    expect(1-2).toBeLessThan(2)
+    expect(1-2).toBeGreaterThan(-2)
+    expect(1-2).toBe(-1)
+  })
+
+  it('can multiply', () => {
+    expect(3*2).toEqual(6)
+  })
+
+  it('can divide', () => {
+    expect(4/2).not.toEqual(3)
+    expect(4/2).toBe(2)
+  })
+
+})
+
