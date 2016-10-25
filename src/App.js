0f81c493fc2c6dfb40b4c635cdd0a366b536c93b
Nate Nichols <natsamnic@gmail.com>
2016-10-24 20:44:52 -0700

    Thanks @mtomcal for your teaching.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Router, Route, Link } from 'react-router'
 
+import './App.css';
 import {Login, Footer} from './Components.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      luke: '',
+      isLoading: true
+    }
+  }
+  componentDidMount(){
+    // overrides base class method, sets state
+    axios.get('/api/people/1')
+    .then((resp) => {
+      this.setState({luke: resp.data, isLoading:false})
+    })
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           HANSEL
         </div>
+        {
+          (this.state.isLoading) ? 'using the force...' : <p>{this.state.luke.name}</p>
+        }
         <Login />
         <Footer />
 
--- src/Components.js
+++ src/Components.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-class Footer extends Component { 
+export class Footer extends Component { 
   render() { 
     return (
       <div className="footer">env: <b>{process.env.NODE_ENV}</b></div>
@@ -8,26 +8,36 @@ class Footer extends Component {
   } 
 }
 
-class Login extends Component {
+export class Login extends Component {
   constructor(props){
     super(props)
     this.state = {username: "", password: ""} 
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(fieldName) {
+    return (event) => {
+      this.setState({[fieldName]: event.target.value})
+    }
   }
 
   handleSubmit(event) {
-    debugger;
+    // if function is not .bind() above, it would look at "this" for the function not the class
   }
   
   render() {
+    let nicename = (this.state.username) ? <p><b>{this.state.username}</b> is such a nice name.</p> : ""
     return (
       <div className="Login">
-        <input type="text" placeholder="username"/><br/>
-        <input type="password" placeholder="password"/><br/>
+        <input value={this.state.username}
+          onChange={this.handleChange("username")}
+          type="text" placeholder="username"/><br/>
+        <input value={this.state.password}
+          onChange={this.handleChange("password")} type="password" placeholder="password"/><br/>
         <button onClick={this.handleSubmit} className="button">login</button>
+        {nicename}
       </div>
     )
   }
 }
-
-export {Login, Footer}
